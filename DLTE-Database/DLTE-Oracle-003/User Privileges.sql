ba65cc655a2f5b358a91437ab4a1ecc413a4951f
Ankitha30 <ankithabhatkl432@gmail.com>
2024-02-24 23:20:27 +0530

    DCL related query

---
+++ DLTE-Database/DLTE-Oracle-003/User Privileges.sql
@@ -0,0 +1,14 @@
+create user Eeksha identified by eeksha;
+grant select on Transaction to Eeksha;
+
+create user ankitha identified by ankitha;
+grant delete on Transaction to ankitha;
+
+create user Divija identified by divija;
+grant select on Transaction to Divija;
+
+create user Sunidhi identified by suni;
+grant insert on Transaction to Sunidhi;
+
+create user Jayashri identified by 1234;
+grant update on Transaction to Jayashri;
