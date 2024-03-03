67fde5e5d26f40ec255968cadf5cf11098d20ca9
Ankitha30 <98896844+Ankitha30@users.noreply.github.com>
2024-03-03 15:16:55 +0530

    Delete DLTE-Java/DLTE-Java-007 directory

--- DLTE-Java/DLTE-Java-007/src/main/java/dynamic/framework/ArrayListIllustrationjava.java
+++
@@ -1,9 +0,0 @@
-package dynamic.framework;
-
-import java.util.ArrayList;
-
-public class ArrayListIllustrationjava {
-    public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList(); // generic
-    }
-}
