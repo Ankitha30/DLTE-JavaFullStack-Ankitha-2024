eede02ecb7fe2eed3582022a1b5e30f5dd41af98
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-06 23:34:31 +0530

    Updated program

--- DLTE-Java/DLTE-Java-002/src/main/java/basics/service/IncomeSlab.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/basics/service/IncomeSlab.java
@@ -4,51 +4,79 @@ import java.util.Scanner;
 
 public class IncomeSlab {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Double salary=0.0,oldRegime=0.0,newRegime=0.0;
-
+        System.out.println("1. New Regime   2.Old Regime");
+        int choice=scanner.nextInt();
         System.out.println("------Welcome to Income Slab------");
         System.out.println("Enter your Annual Income(CTC)");
-        salary=s.nextDouble();
-        if(salary<250000)
-            System.out.println("Exempt in both old and new regims");
-        else if(salary>=250000 && salary<500000){
-            oldRegime=salary*0.05;
-            newRegime=salary*0.05;
-            System.out.println("In old regime tax slab will be "+oldRegime);
-            System.out.println("In new regime tax slab will be "+newRegime);
+        salary=scanner.nextDouble();
+        switch (choice){
+            case 1:
+                    if(salary<250000)
+                        System.out.println("Exempt in new regims");
+                    else if(salary>=250000 && salary<500000){
+                        newRegime=salary*0.05;
+                        System.out.println("In new regime tax slab will be "+newRegime);
+                    }
+                    else if(salary>=50000 && salary<750000){
+                        newRegime=salary*0.05;
+                        System.out.println("In new regime tax slab will be "+newRegime);
+                    }
+                    else if(salary>=750000 && salary<1000000){
+                        newRegime=salary*0.05;
+                        System.out.println("In new regime tax slab will be "+newRegime);
+                    }
+                    else if(salary>=1000000 && salary<1250000){
+                        newRegime=salary*0.05;
+                        System.out.println("In new regime tax slab will be "+newRegime);
+                    }
+                    else if(salary>=1250000 && salary<1500000){
+                        newRegime=salary*0.05;
+                        System.out.println("In new regime tax slab will be "+newRegime);
+                    }
+                    else{
+                        newRegime=salary*0.05;
+                        System.out.println("In new regime tax slab will be "+newRegime);
+                    }
+                    break;
+            case 2:  if(salary<250000)
+                System.out.println("Exempt in both old and new regims");
+            else if(salary>=250000 && salary<500000){
+                oldRegime=salary*0.05;
+                System.out.println("In old regime tax slab will be "+oldRegime);
+
+            }
+            else if(salary>=50000 && salary<750000){
+                oldRegime=salary*0.05;
+                System.out.println("In old regime tax slab will be "+oldRegime);
+            }
+            else if(salary>=750000 && salary<1000000){
+                oldRegime=salary*0.05;
+                System.out.println("In old regime tax slab will be "+oldRegime);
+
+            }
+            else if(salary>=1000000 && salary<1250000){
+                oldRegime=salary*0.05;
+                System.out.println("In old regime tax slab will be "+oldRegime);
+
+            }
+            else if(salary>=1250000 && salary<1500000){
+                oldRegime=salary*0.05;
+                System.out.println("In old regime tax slab will be "+oldRegime);
+
+            }
+            else{
+                oldRegime=salary*0.05;
+                System.out.println("In old regime tax slab will be "+oldRegime);
+
+            }
+            break;
+            default:return;
+
         }
-        else if(salary>=50000 && salary<750000){
-            oldRegime=salary*0.05;
-            newRegime=salary*0.05;
-            System.out.println("In old regime tax slab will be "+oldRegime);
-            System.out.println("In new regime tax slab will be "+newRegime);
-        }
-        else if(salary>=750000 && salary<1000000){
-            oldRegime=salary*0.05;
-            newRegime=salary*0.05;
-            System.out.println("In old regime tax slab will be "+oldRegime);
-            System.out.println("In new regime tax slab will be "+newRegime);
-        }
-        else if(salary>=1000000 && salary<1250000){
-            oldRegime=salary*0.05;
-            newRegime=salary*0.05;
-            System.out.println("In old regime tax slab will be "+oldRegime);
-            System.out.println("In new regime tax slab will be "+newRegime);
-        }
-        else if(salary>=1250000 && salary<1500000){
-            oldRegime=salary*0.05;
-            newRegime=salary*0.05;
-            System.out.println("In old regime tax slab will be "+oldRegime);
-            System.out.println("In new regime tax slab will be "+newRegime);
-        }
-        else{
-            oldRegime=salary*0.05;
-            newRegime=salary*0.05;
-            System.out.println("In old regime tax slab will be "+oldRegime);
-            System.out.println("In new regime tax slab will be "+newRegime);
-        }
-        s.close();;
+
+
 
     }
 
