144db5455ffe5c158c0e471579304c76883fea1b
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-07 00:06:24 +0530

    Updated

--- DLTE-Database/DLTE-Oracle-001/DLTE-Oracle-001.sql
+++ DLTE-Database/DLTE-Oracle-001/DLTE-Oracle-001.sql
@@ -6,8 +6,13 @@ insert into Transaction values(3,'22-Feb-2024','Sunidhi',1500,'Friend');
 insert into Transaction values(4,'12-Feb-2024','Sunidhi',500,'Friend');
 insert into Transaction values(5,'15-Feb-2024','Suvi',900,'Emergency');
 
-1)select * from Transaction where transaction_date between '10-feb-2024' and '25-mar-2024';
-2)select min(transaction_amount) from Transaction;
-3)select max(transaction_amount) from Transaction;
-4)select count(transaction_to) from Transaction where transaction_to='Sunidhi';
+1)Create view dateFilter as select * from Transaction where transaction_date between '10-feb-2024' and '25-mar-2024';
+ View created
+2)create view minimumAmountselect as select min(transaction_amount) from Transaction;
+ View created
+3)create view maxAmount as select max(transaction_amount) from Transaction;
+ View created
+4)create view transactionCount as select count(transaction_to) from Transaction where transaction_to='Sunidhi';
+ View created
 5)select * from Transaction where transaction_remarks='Friend';
+ View created
