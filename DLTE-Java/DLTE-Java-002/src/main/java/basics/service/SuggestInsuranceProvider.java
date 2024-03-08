c804f6a1c7b002703737be63a4e11a1dace5f33c
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-08 21:16:10 +0530

    Updated

--- DLTE-Java/DLTE-Java-002/src/main/java/basics/service/SuggestInsuranceProvider.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/basics/service/SuggestInsuranceProvider.java
@@ -1,39 +1,54 @@
 package basics.service;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.Scanner;
-
 public class SuggestInsuranceProvider {
     public static void main(String[] args) {
-        String bajajAllianzInurance[]={"health checkup","tax benefits","hospitalization costs"};
-        String kotakMahindraInsurance[]={"premium","alternative treatment","health checkup"};
-        String reliance[]={"health checkup","premium","hospitalization cost"};
-        String newFeature="",suggestedCompany="";
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the feature");
-        newFeature=s.nextLine();
-        for(int index=0;index<bajajAllianzInurance.length;index++)
-        {
-            if(newFeature.toLowerCase().contains(bajajAllianzInurance[index]))
-                suggestedCompany+="bajajAllianzInurance ";
-
+    String bajajAllianzInurance[]={"health checkup","tax benefits","hospitalization costs"};
+    String kotakMahindraInsurance[]={"premium","alternative treatment","health checkup"};
+    String reliance[]={"health checkup","premium","hospitalization cost"};
+    for(String each:args){
+        for(String feature:bajajAllianzInurance){
+            if(each.equalsIgnoreCase(feature))
+                System.out.println("Bajaj Allianz Inurance Offers: "+feature);
         }
-        for(int index=0;index<kotakMahindraInsurance.length;index++)
-        {
-            if(newFeature.toLowerCase().contains(kotakMahindraInsurance[index]))
-                suggestedCompany+="kotakMahindraInsurance ";
-
-        }
-        for(int index=0;index<reliance.length;index++)
-        {
-            if(newFeature.toLowerCase().contains(reliance[index]))
-                suggestedCompany+="reliance ";
-
-        }
-        System.out.println("Companies Suggeste dbased on given features:"+suggestedCompany);
-        s.close();
     }
-
-
+    for(String each:args){
+        for(String feature:kotakMahindraInsurance){
+            if(each.equalsIgnoreCase(feature))
+                System.out.println("Kotak Mahindra Insurance Offers: "+feature);
+        }
+    }
+    for(String each:args){
+        for(String feature:reliance){
+            if(each.equalsIgnoreCase(feature))
+                System.out.println("Reliance Offers: "+feature);
+        }
+    }
+ }
+    //        String newFeature="",suggestedCompany="";
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter the feature");
+//        newFeature=s.nextLine();
+//        for(int index=0;index<bajajAllianzInurance.length;index++)
+//        {
+//            if(newFeature.toLowerCase().contains(bajajAllianzInurance[index]))
+//                suggestedCompany+="bajajAllianzInurance ";
+//
+//        }
+//        for(int index=0;index<kotakMahindraInsurance.length;index++)
+//        {
+//            if(newFeature.toLowerCase().contains(kotakMahindraInsurance[index]))
+//                suggestedCompany+="kotakMahindraInsurance ";
+//
+//        }
+//        for(int index=0;index<reliance.length;index++)
+//        {
+//            if(newFeature.toLowerCase().contains(reliance[index]))
+//                suggestedCompany+="reliance ";
+//
+//        }
+//        System.out.println("Companies Suggeste dbased on given features:"+suggestedCompany);
+//        s.close();
+//    }
 }
+
+
