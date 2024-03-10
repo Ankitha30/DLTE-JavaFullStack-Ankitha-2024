1cd54f7adec42093fc39ca68161ea345389acfc7
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-10 18:33:34 +0530

    Updated

--- DLTE-Java/DLTE-Java-Tasks/Testing Dynamic Loan Entity/src/test/java/org/example/AppTest.java
+++ DLTE-Java/DLTE-Java-Tasks/Testing Dynamic Loan Entity/src/test/java/org/example/AppTest.java
@@ -2,40 +2,60 @@ package org.example;
 
 import static org.example.MyBank.loans;
 import static org.example.Loan.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    MyBankMethods app = new  MyBankMethods();
+    MyBankMethods myBank;
+    static List<Loan> list=new ArrayList<>();
+    @Before
+    public void setUp(){
+        myBank =new  MyBankMethods();
+    }
     @BeforeClass
     public static void initialize() {
-        loans.add(new Loan(1234, 30000.0, new Date("3/2/2021"), "open", "Anu", 7435511150L));
-        loans.add(new Loan(2345, 3400.0, new Date("3/4/2022"), "closed", "Ankitha", 9963772901L));
+        list.add(new Loan(1234, 30000.0, new Date("3/2/2021"), "open", "Anu", 7435511150L));
+        list.add(new Loan(2345, 3400.0, new Date("3/4/2022"), "closed", "Ankitha", 9963772901L));
     }
+
+
     @Test
     public void testData(){
-     assertEquals("Loan{loanNumber=1234, loanAmount=30000.0, loanDate='Tue Mar 02 00:00:00 IST 2021', loanStatus='open', borrowerName='Anu', phoneNumber=7435511150}", loans.get(0));
+    assertEquals("Anu",list.get(0).getBorrowerName() );
+    }
+
+    @Test
+    public void testIsEmptyList(){
+        assertNotNull(list);
+//        assertNull(list);
     }
 
     @Test
     public void testCheckOpenLoans() {
-        assertEquals("loanNumber=1234, loanAmount=30000.0, loanDate=new Date(\"3/2/2021\"), loanStatus=open, borrowerName=Anu, phoneNumber=7435511150",app.checkAvailableLoans());
+//        assertEquals("open",list.get(1).getLoanStatus());
+        assertEquals("open",list.get(0).getLoanStatus());
     }
 
+    @Test(timeout = 2000)
+    public void testTimeout() throws InterruptedException {
+        Thread.sleep(90);
+        assertTrue(list.size()>0);
+    }
     @Test
     public void testClosedLoans(){
-        assertEquals("Loan{loanNumber=1234, loanAmount=30000.0, loanDate=new Date(\"3/2/2021\"), loanStatus='open', borrowerName='Anu', phoneNumber=7435511150}",app.checkClosedLoans());
+        assertEquals("closed",list.get(1).getLoanStatus());
+//        assertNotEquals("closed", list.get(0).getLoanStatus());
 
     }
 
