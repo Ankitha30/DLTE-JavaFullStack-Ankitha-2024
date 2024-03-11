863d1197a4c80209dbae86e82b10f75ae6a35089
Ankitha30 <98896844+Ankitha30@users.noreply.github.com>
2024-03-11 22:41:37 +0530

    Updated

--- DLTE-Java/DLTE-Java-002/src/main/java/basics/service/IncomeSlab.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/basics/service/IncomeSlab.java
@@ -20,23 +20,23 @@ public class IncomeSlab {
                         System.out.println("In new regime tax slab will be "+newRegime);
                     }
                     else if(salary>=50000 && salary<750000){
-                        newRegime=salary*0.05;
+                        newRegime=salary*0.1;
                         System.out.println("In new regime tax slab will be "+newRegime);
                     }
                     else if(salary>=750000 && salary<1000000){
-                        newRegime=salary*0.05;
+                        newRegime=salary*0.15;
                         System.out.println("In new regime tax slab will be "+newRegime);
                     }
                     else if(salary>=1000000 && salary<1250000){
-                        newRegime=salary*0.05;
+                        newRegime=salary*0.2;
                         System.out.println("In new regime tax slab will be "+newRegime);
                     }
                     else if(salary>=1250000 && salary<1500000){
-                        newRegime=salary*0.05;
+                        newRegime=salary*0.25;
                         System.out.println("In new regime tax slab will be "+newRegime);
                     }
                     else{
-                        newRegime=salary*0.05;
+                        newRegime=salary*0.3;
                         System.out.println("In new regime tax slab will be "+newRegime);
                     }
                     break;
@@ -44,31 +44,31 @@ public class IncomeSlab {
                 System.out.println("Exempt in both old and new regims");
             else if(salary>=250000 && salary<500000){
                 oldRegime=salary*0.05;
-                System.out.println("In old regime tax slab will be "+oldRegime);
+                System.out.println("In old regime tax slab(5%)  for above 250000 and below 500000 will be "+oldRegime);
 
             }
-            else if(salary>=50000 && salary<750000){
-                oldRegime=salary*0.05;
-                System.out.println("In old regime tax slab will be "+oldRegime);
+            else if(salary>=500000 && salary<7500000){
+                oldRegime=salary*0.2;
+                System.out.println("In old regime tax slab(20%) for above 500000 and below 7500000 will be "+oldRegime);
             }
             else if(salary>=750000 && salary<1000000){
-                oldRegime=salary*0.05;
-                System.out.println("In old regime tax slab will be "+oldRegime);
+                oldRegime=salary*0.2;
+                System.out.println("In old regime tax slab(20%) for above 7500000 and below 1000000 will be "+oldRegime);
 
             }
             else if(salary>=1000000 && salary<1250000){
-                oldRegime=salary*0.05;
-                System.out.println("In old regime tax slab will be "+oldRegime);
+                oldRegime=salary*0.3;
+                System.out.println("In old regime tax slab(30%) for above 1000000 and below 1250000 will be "+oldRegime);
 
             }
             else if(salary>=1250000 && salary<1500000){
-                oldRegime=salary*0.05;
-                System.out.println("In old regime tax slab will be "+oldRegime);
+                oldRegime=salary*0.3;
+                System.out.println("In old regime tax slab(30%) for above 1250000 and below 1500000 will be "+oldRegime);
 
             }
             else{
-                oldRegime=salary*0.05;
-                System.out.println("In old regime tax slab will be "+oldRegime);
+                oldRegime=salary*0.3;
+                System.out.println("In old regime tax slab  for above 1500000 will be "+oldRegime);
 
             }
             break;
