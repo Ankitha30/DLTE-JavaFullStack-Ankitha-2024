b817740e94c2b5edfe7d2aa2fc903393b2d0a978
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-20 22:05:56 +0530

    SpringBoot with JPA

---
+++ DLTE-Spring/Spring-Tasks/Spring with JPA/src/main/java/spring/rest/jpa/ormjparest/SpringWithJpaApplication.java
@@ -0,0 +1,13 @@
+package spring.rest.jpa.ormjparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithJpaApplication.class, args);
+    }
+
+}
---
+++ DLTE-Spring/Spring-Tasks/Spring with JPA/src/test/java/spring/rest/jpa/ormjparest/SpringWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.rest.jpa.ormjparest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
