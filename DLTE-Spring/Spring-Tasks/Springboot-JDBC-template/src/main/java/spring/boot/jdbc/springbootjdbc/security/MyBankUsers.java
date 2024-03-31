1d672f68a71c7f7ba76ac87febe2b0089b9033c6
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-31 12:11:57 +0530

    updated security

--- DLTE-Spring/Spring-Tasks/Springboot-JDBC-template/src/main/java/spring/boot/jdbc/springbootjdbc/security/MyBankUsers.java
+++ DLTE-Spring/Spring-Tasks/Springboot-JDBC-template/src/main/java/spring/boot/jdbc/springbootjdbc/security/MyBankUsers.java
@@ -7,9 +7,25 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class MyBankUsers implements UserDetails {
-    //name, username, password, email, contact, aadhaar
-    private String name,username,password,email;
-    private long contact,aadhaar;
+
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private Long contact;
+    private Long aadhaar;
+
+    public MyBankUsers() {
+    }
+
+    public MyBankUsers(String name, String username, String password, String email, Long contact, Long aadhaar) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.contact = contact;
+        this.aadhaar = aadhaar;
+    }
 
     public String getName() {
         return name;
@@ -19,51 +35,8 @@ public class MyBankUsers implements UserDetails {
         this.name = name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getContact() {
-        return contact;
-    }
-
-    public void setContact(long contact) {
-        this.contact = contact;
-    }
-
-    public long getAadhaar() {
-        return aadhaar;
-    }
-
-    public void setAadhaar(long aadhaar) {
-        this.aadhaar = aadhaar;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
@@ -84,5 +57,58 @@ public class MyBankUsers implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getContact() {
+        return contact;
+    }
+
+    public void setContact(Long contact) {
+        this.contact = contact;
+    }
+
+    public Long getAadhaar() {
+        return aadhaar;
+    }
+
+    public void setAadhaar(Long aadhaar) {
+        this.aadhaar = aadhaar;
+    }
+
+    @Override
+    public String toString() {
+        return "MyBankUsers{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", contact='" + contact + '\'' +
+                ", aadhaar=" + aadhaar +
+                '}';
     }
 }
