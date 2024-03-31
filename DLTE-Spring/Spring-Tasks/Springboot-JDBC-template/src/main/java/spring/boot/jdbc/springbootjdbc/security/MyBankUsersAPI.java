a55f083484866aea68883009f61f3ebef106c9c7
Ankitha30 <ankithabhatkl432@gmail.com>
2024-03-31 11:46:35 +0530

    updated security

--- DLTE-Spring/Spring-Tasks/Springboot-JDBC-template/src/main/java/spring/boot/jdbc/springbootjdbc/security/MyBankUsersAPI.java
+++ DLTE-Spring/Spring-Tasks/Springboot-JDBC-template/src/main/java/spring/boot/jdbc/springbootjdbc/security/MyBankUsersAPI.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/profile")
 public class MyBankUsersAPI {
     @Autowired
     MyBankUserService service;
